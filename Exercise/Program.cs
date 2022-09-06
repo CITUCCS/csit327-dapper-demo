60b2181d0d4e76da96abde2b3525a035723666c8
Jhon Christian Ambrad <jhonchristian.ambrad@cit.edu>
2022-09-06 10:05:04 +0800

    Clean

--- Exercise/Program.cs
+++ Exercise/Program.cs
@@ -1,22 +1,7 @@
-﻿using DapperDemo.Repositories;
-
+﻿
 internal class Program
 {
     private static void Main(string[] args)
     {
-        ISchoolRepository repository = new SchoolRepository();
-        
-        // sample printing
-        var testSchool = repository.GetSchool(1);
-        Console.WriteLine(testSchool);
-
-        // TODO: Do this workflow in Main
-        // 1. Add new school UV
-        // 2. Print id of new school UV
-        // 3. Get new school UV using id and print
-        // 4. Update school UV's motto
-        // 5. GetAllSchools, loop each school and print
-        // 6. Delete school UV
-        // 7. GetAllSchools, loop each school and print again
     }
 }
